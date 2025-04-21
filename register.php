9af06e2c2764fcef0e71c2d27e1da89b7f3119bd
fahadali297t <fahadali297t@gmail.com>
2025-04-21 16:00:09 +0500

    login_register title updated

--- register.php
+++ register.php
@@ -11,7 +11,7 @@ if (isset($_SESSION['user_arr'])) {
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Bootstrap demo</title>
+    <title>Blog_web Register</title>
     <link
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css"
         rel="stylesheet"
